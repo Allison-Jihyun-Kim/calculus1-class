afefe8c49817e614b342ade213bbb04ddf122e81
Allison-Jihyun-Kim <98254127+Allison-Jihyun-Kim@users.noreply.github.com>
2026-08-25 10:52:53 +0900

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 // Google Apps Script 웹 앱 주소를 아래에 붙여 넣으세요.
 // 예: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
 window.CALCULUS_ACTIVITY_CONFIG = {
-  GOOGLE_APPS_SCRIPT_WEB_APP_URL: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE"
+  GOOGLE_APPS_SCRIPT_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyrqidIEcqfYHyZdRi7yQM4YCjazYNtuD3jdvLyszXJwFKvJ7tKu-v_54beXq4oyJfOtA/exec"
 };
